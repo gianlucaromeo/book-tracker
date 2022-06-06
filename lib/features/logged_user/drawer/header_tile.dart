d9b509e9bf663075784a2e6a565b1f81608d5d14
gianlucaromeo <gianlucaromeo@outlook.com>
2022-06-06 19:06:49 +0200

    Update header_tile.dart

--- lib/features/logged_user/drawer/header_tile.dart
+++ lib/features/logged_user/drawer/header_tile.dart
@@ -4,9 +4,8 @@ import 'package:book_tracker/theme/theme_controller.dart';
 import 'package:book_tracker/util/transparent_divider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:lottie/lottie.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AppDrawerHeader extends StatefulWidget {
   const AppDrawerHeader({Key? key}) : super(key: key);
